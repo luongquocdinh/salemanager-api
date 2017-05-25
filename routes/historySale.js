c1e9b607e2e436c0c8e4afe65512cda1929debb5
quocdinhcs <luongquocdinh.95@gmail.com>
2017-05-25 19:48:36 +0700

    history sale

--- routes/historySale.js
+++ routes/historySale.js
@@ -23,7 +23,6 @@ router.post('/add', (req, res) => {
     var saleCode = req.body.saleCode
     var customer = req.body.customer
     var productId = req.body.productId
-
     Product.findOne({_id: productId})
         .then(r => {
             var data = historySale({
@@ -35,7 +34,6 @@ router.post('/add', (req, res) => {
                 price: r.price,
                 bonus: r.bonus
             })
-            console.log(data)
             data.save(function (err) {
                 if (err) {
                     return res.json(responseError("Add history Sale error"))
@@ -99,8 +97,79 @@ router.get('/getlist', (req, res) => {
         })
 })
 
-router.get('/getByDate', (req, res) => {
-    
+router.post('/getByDate', (req, res) => {
+    var dateStart = new Date(req.body.day)
+    var dateEnd = new Date(req.body.day)
+    dateEnd.setHours(dateEnd.getHours() +24);
+    historySale.find({
+        "saleDate": {
+            $gte: new Date(dateStart),
+            $lte: new Date(dateEnd)
+        }
+    })
+    .lean()
+    .exec()
+    .then(data => {
+        return res.json({
+            data: data,
+            error: null
+        })
+    }).catch(err =>{
+        return res.json({
+            data: null,
+            error: err
+        })
+    })
+})
+
+router.post('/getByMonth', (req, res) => {
+    var month = req.body.month
+    var result = []
+    historySale.find({})
+            .lean()
+            .exec()
+            .then(data => {
+                for (var i = 0; i < data.length; i++) {
+                    let t = new Date(data[i].saleDate).getMonth()
+                    if (t + 1 == month) {
+                        result.push(data[i])
+                    }
+                }
+                return res.json({
+                    data: result,
+                    error: null
+                })
+            }).catch(err =>{
+                return res.json({
+                    data: null,
+                    error: err
+                })
+            })
+})
+
+router.post('/getByYear', (req, res) => {
+    var year = req.body.year
+    var result = []
+    historySale.find({})
+            .lean()
+            .exec()
+            .then(data => {
+                for (var i = 0; i < data.length; i++) {
+                    let t = new Date(data[i].saleDate).getFullYear()
+                    if (t == year) {
+                        result.push(data[i])
+                    }
+                }
+                return res.json({
+                    data: result,
+                    error: null
+                })
+            }).catch(err =>{
+                return res.json({
+                    data: null,
+                    error: err
+                })
+            })
 })
 
 module.exports = router
